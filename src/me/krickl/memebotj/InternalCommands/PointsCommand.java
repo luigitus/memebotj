a3793bf7428a0775deb78fe964f6e7ef270e2c04
unlink <lukaskrickl@gmail.com>
2015-12-09 09:25:58 +0100

    Fixed points send

--- src/me/krickl/memebotj/InternalCommands/PointsCommand.java
+++ src/me/krickl/memebotj/InternalCommands/PointsCommand.java
@@ -12,7 +12,8 @@ public class PointsCommand extends CommandHandler {
 
 	@Override
 	public void commandScript(UserHandler sender, ChannelHandler channelHandler, String[] data) {
-		if (channelHandler.getUserList().containsKey(sender.getUsername())) {
+		this.setSuccess(false);
+		if (channelHandler.getUserList().containsKey(sender.getUsername().toLowerCase())) {
 			if (data.length < 1) {
 				channelHandler.sendMessage(String.format("%s: %.2f %s", sender.getUsername(), channelHandler.getUserList().get(sender.getUsername()).getPoints(), channelHandler.getBuiltInStrings().get("CURRENCY_EMOTE")), this.getChannelOrigin());
 			} else {
@@ -32,27 +33,31 @@ public class PointsCommand extends CommandHandler {
                         double number = Double.parseDouble(data[2]);
                         if (data[0].equals("add")) {
                             target.setPoints(target.getPoints() + number);
+                            this.setSuccess(true);
                         } else if (data[0].equals("sub")) {
                             target.setPoints(target.getPoints() - number);
+                            this.setSuccess(true);
                         } else if (data[0].equals("set")) {
                             target.setPoints(number);
+                            this.setSuccess(true);
                         }
 
-                        channelHandler.sendMessage(
-                                String.format("%s your new total is: %.2f %s", target.getUsername(), target.getPoints(),
-                                        channelHandler.getBuiltInStrings().get("CURRENCY_EMOTE")),
-                                this.getChannelOrigin());
-                        this.setSuccess(true);
+                        if(this.getSuccess()) {
+                            channelHandler.sendMessage(
+                                    String.format("%s your new total is: %.2f %s", target.getUsername(), target.getPoints(),
+                                            channelHandler.getBuiltInStrings().get("CURRENCY_EMOTE")),
+                                    this.getChannelOrigin());
+                        }
                     }
 
-                    if (target != null && !success()) {
+                    if (target != null && !this.getSuccess()) {
                         double number = Double.parseDouble(data[2]);
                         double tax = number / 100 * 10;
 						if(data[0].equals("send")) {
                             if(this.checkCost(sender, number + tax, channelHandler)) {
                                 sender.setPoints(sender.getPoints() - (number + tax));
-                                target.setPoints(target.getPoints() + number + tax);
-                                channelHandler.sendMessage(String.format("%s: You sent %.2f %s to %s", target.getUsername(), number + tax,
+                                target.setPoints(target.getPoints() + number);
+                                channelHandler.sendMessage(String.format("%s: You sent %.2f %s to %s", target.getUsername(), number,
                                         channelHandler.getBuiltInStrings().get("CURRENCY_EMOTE"), target.getUsername()), this.getChannelOrigin());
                             }
                         } else {
