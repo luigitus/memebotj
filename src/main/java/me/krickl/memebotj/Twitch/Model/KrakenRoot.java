e0a25983a7a9a8ee6e5311650547538b54ff8e3d
Luigitus <riseyu@posteo.de>
2016-05-15 16:22:36 +0200

    Added Models for Twitch Kraken root

--- src/main/java/me/krickl/memebotj/Twitch/Model/KrakenRoot.java
+++ src/main/java/me/krickl/memebotj/Twitch/Model/KrakenRoot.java
@@ -1,7 +1,35 @@
 package me.krickl.memebotj.Twitch.Model;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
 /**
- * Created by nfass on 13.05.16.
+ * This file is part of memebotj.
+ * Created by Luigitus on 15/05/16.
  */
 public class KrakenRoot {
+
+    @SerializedName("identified")
+    @Expose
+    private Boolean identified;
+    @SerializedName("token")
+    @Expose
+    private KrakenToken token;
+
+    public Boolean getIdentified() {
+        return identified;
+    }
+
+    public void setIdentified(Boolean identified) {
+        this.identified = identified;
+    }
+
+    public KrakenToken getToken() {
+        return token;
+    }
+
+    public void setToken(KrakenToken token) {
+        this.token = token;
+    }
+
 }
--- src/main/java/me/krickl/memebotj/Twitch/Model/KrakenToken.java
+++ src/main/java/me/krickl/memebotj/Twitch/Model/KrakenToken.java
@@ -1,7 +1,46 @@
 package me.krickl.memebotj.Twitch.Model;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
 /**
- * Created by nfass on 13.05.16.
+ * This file is part of memebotj.
+ * Created by Luigitus on 15/05/16.
  */
 public class KrakenToken {
+
+    @SerializedName("valid")
+    @Expose
+    private Boolean valid;
+    @SerializedName("authorization")
+    @Expose
+    private KrakenAuthorization authorization;
+    @SerializedName("user_name")
+    @Expose
+    private String userName;
+
+    public Boolean getValid() {
+        return valid;
+    }
+
+    public void setValid(Boolean valid) {
+        this.valid = valid;
+    }
+
+    public KrakenAuthorization getAuthorization() {
+        return authorization;
+    }
+
+    public void setAuthorization(KrakenAuthorization authorization) {
+        this.authorization = authorization;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
 }
